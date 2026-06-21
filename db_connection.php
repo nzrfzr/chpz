8a4c43e773cec222486027cc668a90b4a3062309
Nizar Fazari <nizarfa1811@gmail.com>
2026-06-21 10:55:33 +0800

    update username dan password database

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
 // Establish a connection to the database
 $servername = "localhost";
-$username = "cheapiza";
-$password = "cheapz2026";
+$username = "root";
+$password = "";
 $dbname = "cheapiza_fastfood";
 
 // Enable error reporting
